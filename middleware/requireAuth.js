80e152df92f2a787a877556312f0f5403d9fd7a2
Valiant Joe <highscoretechng@gmail.com>
2023-02-24 14:04:53 +0100

    first commit

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -3,23 +3,23 @@ const User = require('../model/users')
 
 const requireAuth = ( async (req, res, next)=>{
 
-    res.json({erorr:"message"})
+    const  {authorization}  = req.headers
 
-    // const  {authorization}  = req.headers
+    if(!authorization){
+        return res.status(401).json({error : "Authorization token required"})
+    }
+    else{
+        const token = authorization.split(" ")[1]
+        try {
+        const  {_id }= jwt.verify(token, process.env.SECRET)
 
-    // if(!authorization){
-    //     return res.status(401).json({error : "Authorization token required"})
-    // }
-    // else{
-    //     const token = authorization.split(" ")[1]
-    //     try {
-    //     const  {_id }= jwt.verify(token, process.env.SECRET)
+        req.user = await User.findOne({_id}).select("_id")
 
-    //     req.user = await User.findOne({_id}).select("_id")
-    //     next()
-    //     } catch (error) {
-    //         res.status(404).json({error: "Request not authorized"})
-    //     }
-    // }
+        res.json({response: "access granted"})
+        // next()
+        } catch (error) {
+            res.status(404).json({error: "Request not authorized"})
+        }
+    }
 })
 module.exports = requireAuth
