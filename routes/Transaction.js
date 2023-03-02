79fe930aad77385176efb481a13de9144e0f2014
Valiant Joe <highscoretechng@gmail.com>
2023-03-02 19:25:13 +0100

    navbar push

--- routes/Transaction.js
+++ routes/Transaction.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const requireAuth = require('../middleware/requireAuth')
 
-const { Deposit, depositHistory, Withdraw } = require('../controller/Transaction')
+const { Deposit, depositHistory, Withdraw, ToMain, ToCoin } = require('../controller/Transaction')
 
 router.use(requireAuth)
 
 //.. Create a new deposit 
 router.post('/deposit', Deposit)
 router.post('/withdraw', Withdraw)
+router.post('/to-main', ToMain)
+router.post('/to-coin', ToCoin)
 
 //.. Get all deposits 
 router.get('/history', depositHistory)
